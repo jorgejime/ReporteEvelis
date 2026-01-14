6a334dea89b31c1b0eaed29ee1a6a4912170d960
Jorge Jimenez Calvano <127059451+jorgejime@users.noreply.github.com>
2026-01-14 11:55:10 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      optimizeDeps: {
+        include: ['jspdf', 'jspdf-autotable']
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
